b799d1b335818bce2f2abcc06f1a6f35ec543571
goutam <goutamr362@gmail.com>
2025-04-21 23:06:35 +0600

    dactor and patient register and dashbord page added

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -6,8 +6,38 @@ use Illuminate\Http\Request;
 
 class FrontendController extends Controller
 {
-    // show home page 
+    // show Home page 
     public function showHomePage(){
         return view("frontend.home");
     }
+
+
+    // show Login page 
+    public function showLoginPage(){
+        return view("frontend.login");
+    }
+
+    // show Patient Register page 
+    public function showPatientRegisterPage(){
+        return view("frontend.patient.register");
+    }
+
+    // show patient Dasboard page 
+    public function showPatientDashboardPage(){
+        return view("frontend.patient.dashboard");
+    }
+
+
+    // show Dactor Register page 
+    public function showDactorRegisterPage(){
+        return view("frontend.dactor.register");
+    }
+
+    // show Dactor Dasboard page 
+    public function showDactorDashboardPage(){
+        return view("frontend.dactor.dashboard");
+    }
+
+
+
 }
--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,13 @@ use App\Http\Controllers\FrontendController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [FrontendController::class, "showHomePage"]) -> name("home.page");
+Route::get('/login', [FrontendController::class, "showLoginPage"]) -> name("login.page");
+
+// patient register route
+Route::get('/patient-register', [FrontendController::class, "showPatientRegisterPage"]) -> name("patientRegister.page");
+Route::get('/patient-dashboard', [FrontendController::class, "showPatientDashboardPage"]) -> name("patientDashboard.page");
+
+
+// dactor register route
+Route::get('/dactor-register', [FrontendController::class, "showDactorRegisterPage"]) -> name("dactorRegister.page");
+Route::get('/dactor-dashboard', [FrontendController::class, "showDactorDashboardPage"]) -> name("dactorDashboard.page");
